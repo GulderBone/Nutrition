a0c0ccfa5200520c087c5bb8953603e7ab8e8506
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-07-16 18:34:44 +0200

    1 | clean up

--- data/src/main/java/com/gulderbone/data/model/ProductResponse.kt
+++ data/src/main/java/com/gulderbone/data/model/ProductResponse.kt
@@ -5,6 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ProductResponse(
-    @field:Json(name = "product_name") val productName: String,
-    @field:Json(name = "categories_tags") val categoriesTags: List<String>,
+    @Json(name = "product_name") val productName: String,
+    @Json(name = "categories_tags") val categoriesTags: List<String>,
 )
