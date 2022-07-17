7c52228c99cd8fee7e6332b8d0429821d3ef6542
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-07-17 05:54:54 +0200

    1 | Add missing inject constructor

--- data/src/main/java/com/gulderbone/data/repository/mapper/ProductStateMapper.kt
+++ data/src/main/java/com/gulderbone/data/repository/mapper/ProductStateMapper.kt
@@ -2,8 +2,9 @@ package com.gulderbone.data.repository.mapper
 
 import com.gulderbone.data.model.ProductStateResponse
 import com.gulderbone.data.repository.Product
+import javax.inject.Inject
 
-class ProductStateMapper {
+class ProductStateMapper @Inject constructor() {
 
     fun from(productStateResponse: ProductStateResponse): Product = with(productStateResponse) {
         Product(
