9576203b0b7660ae5638d36b34986a68dbca2909
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-07-19 16:43:52 +0200

    1 | networking setup (#1)
    * 1 | start
    
    * 1 | networking setup complete with an example.
    
    * 1 | gitignore update
    
    * 1 | clean up
    
    * 1 | formatting
    
    * 1 | clean up
    
    * 1 | refactor and dependency versions extracted
    
    * 1 | Retrofit is not used in this module
    
    * 1 | Dependencies updated.
    
    * 1 | Clean up
    
    * 1 | Extract mapper
    
    * 1 | Add missing inject constructor
    
    * 1 | Add test
    
    * 1 | Extract data class

--- app/src/main/java/com/gulderbone/nutrition/MainActivity.kt
+++ app/src/main/java/com/gulderbone/nutrition/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.gulderbone.nutrition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gulderbone.data.repository.ProductRepository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var productRepository: ProductRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO delete - it is just for testing purposes
+        runBlocking {
+            val product = productRepository.getProductByBarcode("04963406")
+            println(product)
+        }
     }
 }
---
+++ data/src/main/java/com/gulderbone/data/repository/ProductRepository.kt
@@ -0,0 +1,17 @@
+package com.gulderbone.data.repository
+
+import com.gulderbone.data.model.product.Product
+import com.gulderbone.data.repository.mapper.ProductStateMapper
+import com.gulderbone.data.service.ProductService
+import javax.inject.Inject
+
+class ProductRepository @Inject constructor(
+    private val productService: ProductService,
+    private val productStateMapper: ProductStateMapper,
+) {
+
+    suspend fun getProductByBarcode(barcode: String): Product =
+        productService.getProductByBarcode(barcode)
+            .let(productStateMapper::from)
+}
+
