cae9abb4f98c6cebe9a2d67a56c54d23b356e0c2
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-07-23 20:03:03 +0200

    2 | Extract constraint layout version

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,6 +4,7 @@ object Versions {
     const val targetSdk = 32
     const val kotlin = "1.8.0"
     const val appCompat = "1.4.2"
+    const val constraintLayout = "2.1.4"
     const val material = "1.6.1"
     const val moshi = "1.13.0"
     const val retrofit = "2.9.0"
