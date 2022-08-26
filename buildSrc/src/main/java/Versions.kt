c9d5edb016c300c5d72738d00e801cad6759e27f
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-08-26 15:29:06 +0200

    2 | Add presentation module and navigation component dependencies. (#2)
    * 2 | Add presentation module and navigation component dependencies.
    
    * 2 | Add Main fragment
    
    * 2 | Extract constraint layout version
    
    * 2 | empty line

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,10 +4,12 @@ object Versions {
     const val targetSdk = 32
     const val kotlin = "1.8.0"
     const val appCompat = "1.4.2"
+    const val constraintLayout = "2.1.4"
     const val material = "1.6.1"
     const val moshi = "1.13.0"
     const val retrofit = "2.9.0"
     const val hilt = "2.42"
+    const val navigation = "2.5.0"
     const val coroutines = "1.6.3"
     const val junit5 = "5.8.2"
     const val mockitoKotlin = "4.0.0"
