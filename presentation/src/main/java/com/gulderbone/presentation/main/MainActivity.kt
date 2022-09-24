e6f3b1794a2caab5e9d83d13aa08b9c9dbf8a80a
Mateusz Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-09-24 17:25:36 +0200

    4/view binding setup (#4)
    * 5 | ViewBinding
    
    * 5 | CR comments answered
    
    * 5 | internal added before classes and methods, names changed

--- presentation/src/main/java/com/gulderbone/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/gulderbone/presentation/main/MainActivity.kt
@@ -1,18 +1,16 @@
-package com.gulderbone.presentation
+package com.gulderbone.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gulderbone.presentation.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NutritionMainActivity : AppCompatActivity() {
-
-//    @Inject
-//    lateinit var productRepository: ProductRepository
+internal class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nutrition_main)
+        setContentView(R.layout.activity_main)
 
         // TODO delete - it is just for testing purposes
 //        runBlocking {
